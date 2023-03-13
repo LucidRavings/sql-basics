681e76044af907f6af631fbf26acd60bc5e9ea9d
LucidRavings <cybernerd82@yahoo.com>
2023-03-13 16:23:12 -0600

    Completed step 4.

--- artist.sql
+++ artist.sql
@@ -1,1 +1,14 @@
 --Step 4
+
+INSERT INTO artist (name)
+VALUES ('Dogwater'),
+('B.A.D.'),
+('Lucky Knee');
+
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+SELECT * FROM artist ORDER BY name LIMIT 5;
+
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+SELECT * FROM artist WHERE name LIKE '%Black%';
